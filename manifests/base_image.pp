08fcdbddcd867083a2c35ad8d6341c9f1cb57d2d
Charlie Sharpsteen <source@sharpsteen.net>
2020-01-04 13:49:46 -0800

    Re-order Base_image resources
    This commit re-orders the resources in the Base_image type to place
    puppet-agent installation last and adds some comments marking different
    sections. This fixes an issue where puppet-agent installation would
    fail on the first catalog application when /etc/hosts and /etc/resolv.conf
    need to be configured in order for the master's hostname to be resolved.

--- manifests/base_image.pp
+++ manifests/base_image.pp
@@ -25,6 +25,8 @@ define clone_army::base_image (
   ensure_resource('file', $vardir, {ensure => directory})
   $_path = join([$vardir, $title], '/')
 
+  # === Provision sysroot ===
+
   # TODO: Use debootstrap for Debian machines
   # TODO: Use zypper --root for SLES machines
   exec {"create ${title} base container":
@@ -46,6 +48,46 @@ define clone_army::base_image (
       |-EOS
     creates => "${_path}/etc/redhat-release",
   }
+
+  # === Configure networking ===
+
+  file {"${_path}/etc/hosts":
+    ensure  => file,
+    source  => '/etc/hosts',
+    require => Exec["create ${title} base container"],
+  }
+
+  file {"${_path}/etc/resolv.conf":
+    ensure  => file,
+    source  => '/etc/resolv.conf',
+    require => Exec["create ${title} base container"],
+  }
+
+  # === Configure root user login ===
+
+  # Remove securetty to prevent PAM from blocking `machinectl login`
+  file {"${_path}/etc/securetty":
+    ensure  => absent,
+    require => Exec["create ${title} base container"],
+  }
+
+  # Set well-known password for root login via `machinectl login`
+  #
+  # NOTE: Ideally this would be set using pw_hash from stdlib. But, pw_hash()
+  #       on macOS cannot generate Linux hashes, which means `bolt apply`
+  #       does not work. So, we just use a canned hash value.
+  augeas {'clone_army base container root password':
+    context => "/files/${_path}/etc/shadow",
+    incl    => "${_path}/etc/shadow",
+    lens    => 'Shadow.lns',
+    changes => [
+      # "puppetlabs"
+      'set root/password "$5$NaCl$zgOC59aimZi/HwooGdPEyYgn144lBH1dbOxtKjyjIQ3"',
+    ],
+    require => Exec["create ${title} base container"],
+  }
+
+  # === Provision puppet-agent ===
 
   # TODO: Add option to use puppet_agent for FOSS installs
   file {"${_path}/install-puppet.sh":
@@ -76,39 +118,5 @@ define clone_army::base_image (
       |-EOS
     creates => "${_path}/opt/puppetlabs/puppet/VERSION",
     require => File["${_path}/install-puppet.sh"],
-  }
-
-  file {"${_path}/etc/hosts":
-    ensure  => file,
-    source  => '/etc/hosts',
-    require => Exec["create ${title} base container"],
-  }
-
-  file {"${_path}/etc/resolv.conf":
-    ensure  => file,
-    source  => '/etc/resolv.conf',
-    require => Exec["create ${title} base container"],
-  }
-
-  # Remove securetty to prevent PAM from blocking `machinectl login`
-  file {"${_path}/etc/securetty":
-    ensure  => absent,
-    require => Exec["create ${title} base container"],
-  }
-
-  # Set well-known password for root login via `machinectl login`
-  #
-  # NOTE: Ideally this would be set using pw_hash from stdlib. But, pw_hash()
-  #       on macOS cannot generate Linux hashes, which means `bolt apply`
-  #       does not work. So, we just use a canned hash value.
-  augeas {'clone_army base container root password':
-    context => "/files/${_path}/etc/shadow",
-    incl    => "${_path}/etc/shadow",
-    lens    => 'Shadow.lns',
-    changes => [
-      # "puppetlabs"
-      'set root/password "$5$NaCl$zgOC59aimZi/HwooGdPEyYgn144lBH1dbOxtKjyjIQ3"',
-    ],
-    require => Exec["create ${title} base container"],
   }
 }
